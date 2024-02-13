f270f03f517a1f5eb2884560c1a57572e1878447
Felipe Silva <felipesilva2004@hotmail.com>
2024-02-13 13:06:19 -0300

    feat(ActivitiesTable): simple table component + storybook

--- frontend/src/components/ActivitiesTable.jsx
+++ frontend/src/components/ActivitiesTable.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Table } from 'antd';
+
+const columns = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+  },
+  {
+    title: 'Currency',
+    dataIndex: 'currency',
+  },
+  {
+    title: 'Rating',
+    dataIndex: 'rating',
+  },
+  {
+    title: 'Special Offer',
+    dataIndex: 'specialOffer',
+  },
+  {
+    title: 'Supplier ID',
+    dataIndex: 'supplierId',
+  },
+]
 
 const ActivitiesTable = ({ activities }) => {
-  return (<div>Activities Table Here</div>)
+  return (
+    <Table
+      columns={columns}
+      dataSource={activities}
+      rowKey={(record) => record.id}
+    />
+  )
 }
 
 export default ActivitiesTable;
--- frontend/src/components/ActivitiesTable.stories.jsx
+++ frontend/src/components/ActivitiesTable.stories.jsx
@@ -1,8 +1,12 @@
 import ActivitiesTable from "./ActivitiesTable";
+import activities from '../../../resources/activities.json'
 
 export default {
   title: "Components/ActivitiesTable",
   component: ActivitiesTable,
+  args: {
+    activities,
+  },
 }
 
 const Template = (args) => <ActivitiesTable {...args} />;
