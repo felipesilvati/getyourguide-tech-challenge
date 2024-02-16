0aeea7f8b421446ef1e70055a495d4e6df7e6df3
Felipe Silva <felipesilva2004@hotmail.com>
2024-02-16 12:15:37 -0300

    chore: add comments on things that make sense

--- backend/controllers/activity.controller.js
+++ backend/controllers/activity.controller.js
@@ -11,21 +11,21 @@ exports.getActivities = (req, res) => {
 }
 
 exports.getActivitiesWithSupplier = (req, res) => {
+  // Search query
   let filteredActivities = req.query?.query ?
     activities.filter(activity => activity.title.toLowerCase().includes(req.query.query?.toLowerCase()))
     : activities;
 
-  // query parameter onlyShowSpecialOffers is a string, so we need to compare it to the string 'true'
+  // Filter by special offer
   if (req.query?.onlyShowSpecialOffers === 'true') {
     filteredActivities = filteredActivities.filter(activity => activity.specialOffer);
   }
 
+  // Pagination
   const page = parseInt(req.query.page, BASE_TEN_RADIX) || DEFAULT_FIRST_PAGE;
   const pageSize = parseInt(req.query.pageSize, BASE_TEN_RADIX) || DEFAULT_PAGE_SIZE;
-
   const start = (page - DEFAULT_FIRST_PAGE) * pageSize;
   const end = page * pageSize;
-
   const paginatedActivities = filteredActivities.slice(start, end);
 
   res.status(200).json({
